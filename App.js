b54134e338ee9ac2a283956b7f95e58d6d453818
Jayachandranm46 <158546430+Jayachandranm46@users.noreply.github.com>
2025-02-15 16:01:05 +0530

    ddd

---
+++ App.js
@@ -0,0 +1,11 @@
+import { View,Text } from "react-native";
+
+const App=()=>{
+    return(
+        <View>
+            <Text>spp</Text>
+        </View>
+    )
+}
+
+export default App;
---
+++ app.config.js
@@ -0,0 +1,50 @@
+import 'dotenv/config';
+export default{
+  "expo": {
+    "name": "MJChatApp",
+    "slug": "MJChatApp",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/images/icon.png",
+    "scheme": "myapp",
+    "userInterfaceStyle": "automatic",
+    "newArchEnabled": true,
+    "ios": {
+      "supportsTablet": true
+    },
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/images/adaptive-icon.png",
+        "backgroundColor": "#ffffff"
+      }
+    },
+    "web": {
+      "bundler": "metro",
+      "output": "static",
+      "favicon": "./assets/images/favicon.png"
+    },
+    "plugins": [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          "image": "./assets/images/splash-icon.png",
+          "imageWidth": 200,
+          "resizeMode": "contain",
+          "backgroundColor": "#ffffff"
+        }
+      ]
+    ],
+    "experiments": {
+      "typedRoutes": true
+    },
+    extra:{
+      apiKey:process.env.API_KEY,
+      authDomain:process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID
+    }
+  }
+}
